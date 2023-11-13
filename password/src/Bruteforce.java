2c916e99122efe4dcc7488fef557d014da44693d
Adam-090 <adam.hathiyari@gmail.com>
2023-11-13 10:57:33 -0600

    First real commit

---
+++ password/src/Bruteforce.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Bruteforce {
+    int Length;
+    String Word;
+
+    public Bruteforce(String Char, int length){
+        ArrayList<String> Characters = new ArrayList<String>();
+        Length = length;
+        Word = Char;
+
+    }
+    public void append(String word){
+
+    }
+
+
+
+
+
+}
---
+++ password/src/BruteforceRunner.java
@@ -0,0 +1,5 @@
+public class BruteforceRunner {
+    public static void main(String [] args){
+        Bruteforce h = new Bruteforce("h",5);
+    }
+}
