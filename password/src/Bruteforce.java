ff2b71a0c59a2f2529186eea19e44eb3b996aa4b
Adam-090 <adam.hathiyari@gmail.com>
2023-11-15 10:27:55 -0600

    3rd commit

--- password/src/Bruteforce.java
+++ password/src/Bruteforce.java
@@ -3,15 +3,19 @@ import java.util.ArrayList;
 public class Bruteforce {
     int Length;
     String Word;
-
+    ArrayList<String> CharList;
     public Bruteforce(String Char, int length){
-        ArrayList<String> Characters = new ArrayList<String>();
+         CharList = new ArrayList<String>();
         Length = length;
         Word = Char;
+        append(Word);
+        System.out.print(CharList);
 
     }
     public void append(String word){
-
+        for(int i =0; i < Word.length();i++){
+            CharList.add(Word.substring(i,i+1));
+        }
     }
 
 
--- password/src/BruteforceRunner.java
+++ password/src/BruteforceRunner.java
@@ -1,5 +1,5 @@
 public class BruteforceRunner {
     public static void main(String [] args){
-        Bruteforce h = new Bruteforce("h",5);
+        Bruteforce h = new Bruteforce("hello823A",5);
     }
 }
