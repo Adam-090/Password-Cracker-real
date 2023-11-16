becfc60fdc7d39743bdcbe0de93e9137011b00e5
Adam H <adam.hathiyari@gmail.com>
2023-11-16 10:48:13 -0600

    python first

--- password/Passwordpython/BruteForceme.py
+++ password/Passwordpython/BruteForceme.py
@@ -1,4 +1,9 @@
+import itertools
+
 characs = input("Enter the Chracters / numbers used in the password ")
 length = input("enter the password Length in integer form ")
+chracsList = list(itertools.chain.from_iterable(characs))
 print(characs)
 print(length)
+print(chracsList)
+
