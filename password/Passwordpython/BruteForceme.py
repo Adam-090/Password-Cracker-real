3fffee1310c149d423873b31be3817bc846668d0
Adam-090 <adam.hathiyari@gmail.com>
2023-11-28 10:55:33 -0600

    python #3

--- password/Passwordpython/BruteForceme.py
+++ password/Passwordpython/BruteForceme.py
@@ -1,9 +1,18 @@
 import itertools
-
+password = input("Enter the password you want to guess")
 characs = input("Enter the Chracters / numbers used in the password ")
-length = input("enter the password Length in integer form ")
+length = int(input("Enter the password Length in integer form "))
 chracsList = list(itertools.chain.from_iterable(characs))
 print(characs)
 print(length)
 print(chracsList)
+
+changedList = list(itertools.product(chracsList,repeat=length))
+print(changedList)
+
+
+for x in changedList:
+    print(x)
+    if x == password:
+        
 
